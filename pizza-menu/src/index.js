6c08f8042fbf7095343e50a2650dd89fe7069c8d
Deepak <youcansend2ds@gmail.com>
2024-01-02 23:28:51 +0530

    implemented the conditional rendering in the footer

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -72,12 +72,14 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas < 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu, please visit later..</p>
       )}
     </main>
   );
@@ -100,15 +102,19 @@ function Footer() {
   const hour = new Date().getHours();
   const openHour = 10;
   const closeHour = 22;
-  const isOpen = hour >= openHour && hour <= closeHour ? "open" : "closed";
+  const isOpen = hour >= openHour && hour <= closeHour;
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00 .
+        </p>
       )}
     </footer>
   );
