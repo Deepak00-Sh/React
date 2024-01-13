554bc4fe6e0dcb983ce434ff17e87e416c40bdc6
Deepak <youcansend2ds@gmail.com>
2024-01-13 16:09:57 +0530

    Flashcards Exercice

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -19,10 +21,45 @@ function Logo() {
   return <h1>🌴Far Away 🎒</h1>;
 }
 function Form() {
+  //Creating the state for controlling the elements of the form
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  //Event handler to handle the Add click
+  function handleSubmit(e) {
+    // This prevents the page to reload on form submit
+    // e is the object with all the information about the current event
+    e.preventDefault();
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 25 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item...."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
@@ -30,7 +67,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -40,7 +77,7 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity}
         {item.description}
       </span>
---
+++ travel-list/src/README.md
@@ -0,0 +1,37 @@
+<h1>React concepts</h1>
+
+##
+
+<h3> Controlled Elements </h3>
+1. Controlled elements are used when working with the forms in react.
+2. By default, the input fields maintains their own state inside DOM and this make it hard to read their values.
+3. In react, we keep the values at one central place and in order to do that we use <strong>controlled elements</strong>
+
+4. With this technique, it is react which controls and owns the state of these elements.
+
+In order to keep the inputs in sync with the application, we do three steps :
+
+<ul>
+<li> Create a piece of state </li>
+i.e.
+<!-- const [description, setDescription] = useState(""); -->
+
+<li>Implement the state Value to the form element</li>
+i.e.
+<!-- <input
+        type="text"
+        placeholder="Item...."
+        value={description}
+/> -->
+
+<li> Adding the onChange function on the form element to handle the change event else it will remain the empty as defined in the useState("")</li>
+i.e.
+<!-- <input
+        type="text"
+        placeholder="Item...."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      /> -->
+</ul>
