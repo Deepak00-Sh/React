d5097e1ea6cbd95535157a3a74ff56010da3ab6b
Deepak <youcansend2ds@gmail.com>
2024-01-13 18:19:19 +0530

    Implemented the add new Items to the travel-list by using the lift-up state concept of react

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -7,11 +7,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -20,10 +25,12 @@ export default function App() {
 function Logo() {
   return <h1>🌴Far Away 🎒</h1>;
 }
-function Form() {
+
+function Form({ onAddItems }) {
   //Creating the state for controlling the elements of the form
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   //Event handler to handle the Add click
   function handleSubmit(e) {
     // This prevents the page to reload on form submit
@@ -32,7 +39,7 @@ function Form() {
 
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-
+    onAddItems(newItem);
     setDescription("");
     setQuantity(1);
   }
@@ -62,11 +69,11 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
