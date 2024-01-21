a5cd9b401648a50a11cdff02859b19fc35d04ed4
Deepak <youcansend2ds@gmail.com>
2024-01-21 23:31:41 +0530

    Adding the data to create product

--- shopify-app/src/App.js
+++ shopify-app/src/App.js
@@ -1,9 +1,54 @@
+const list = [
+  {
+    name: "Puma Bagback",
+    info: "34L Puma backpack with 5 year of manufacturing warrenty",
+    image:
+      "https://assets.ajio.com/medias/sys_master/root/h50/hcc/16036165320734/-473Wx593H-460571671-black-MODEL.jpg",
+    availableQuantity: 5,
+  },
+  {
+    name: "Armani Exchange",
+    info: "13 Laptop BagPack with Embossed Logo",
+    image:
+      " https://assets.ajio.com/medias/sys_master/root/20231103/pAYM/654525d5afa4cf41f57382d6/-473Wx593H-410394390-00035-MODEL7.jpg",
+    availableQuantity: 2,
+  },
+  {
+    name: "The ClownFish",
+    info: "Genuine Leather Laptop BackPack",
+    image:
+      "https://assets.ajio.com/medias/sys_master/root/20230629/2zkI/649ce1bdeebac147fc348eaa/-473Wx593H-465997434-black-MODEL.jpg",
+    availableQuantity: 6,
+  },
+  {
+    name: "The ClownFish",
+    info: "17'' Laptop Backpack with Adjustable Shoulder Straps",
+    image:
+      "https://assets.ajio.com/medias/sys_master/root/20230629/DMc9/649caceba9b42d15c91669c4/-473Wx593H-465956870-grey-MODEL6.jpg",
+    availableQuantity: 10,
+  },
+  {
+    name: "GODS",
+    info: "Graphic Printed Laptop BackPack",
+    image:
+      "https://assets.ajio.com/medias/sys_master/root/20230628/zCi3/649ba301a9b42d15c9055e6d/-473Wx593H-465691600-black-MODEL.jpg",
+    availableQuantity: 5,
+  },
+  {
+    name: "REDHORNS",
+    info: "Men 16 Striped Anti-Theft Laptop Backpack",
+    image:
+      "https://assets.ajio.com/medias/sys_master/root/20231225/mTls/6589b749afa4cf41f5e9fe9f/-473Wx593H-466912682-silver-MODEL6.jpg",
+    availableQuantity: 2,
+  },
+];
+
 export default function App() {
   return (
-    <div className="h-screen bg-indigo-700">
+    <div className="h-screen bg-zinc-700">
       <Header />
       <div className="p-6 md:p-10 md:m-6">
-        <Product />
+        <ProductList />
       </div>
     </div>
   );
@@ -12,16 +57,24 @@ export default function App() {
 function Header() {
   return (
     <div className="grid place-content-center">
-      <span className="m-6 text-4xl text-white font-bold">Shopify Online</span>
+      <span className="m-6 text-4xl text-white font-bold ">
+        👜 BagPack Station
+      </span>
     </div>
   );
 }
 
-function ProductList() {
-  return <div></div>;
+function ProductList({ list }) {
+  return (
+    <div>
+      {list.map((product) => (
+        <Product product={product} />
+      ))}
+    </div>
+  );
 }
 
-function Product() {
+function Product({ product }) {
   return (
     <div className="w-52 md:w-1/5 bg-white rounded-lg overflow-hidden ">
       <div>
