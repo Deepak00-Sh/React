70450fe25b82e21af9eb4beb7ec1307f18862a80
Deepak <youcansend2ds@gmail.com>
2024-01-28 21:10:58 +0530

    Implementing the onHover and onHoverOut on rating

--- usepopcorn/src/components/StarRating.js
+++ usepopcorn/src/components/StarRating.js
@@ -16,6 +16,7 @@ const textStyle = {
 };
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   return (
     <div style={containerStyle}>
@@ -25,12 +26,14 @@ export default function StarRating({ maxRating = 5 }) {
             <Star
               key={i}
               onRate={() => setRating(i + 1)}
-              full={rating >= i + 1}
+              full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
             />
           </span>
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -42,9 +45,15 @@ const starStyle = {
   cursor: PointerEvent,
 };
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" onClick={onRate} style={starStyle}>
+    <span
+      role="button"
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+      style={starStyle}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
