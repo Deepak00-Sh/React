4fff407680b2b884cc44098218acca5e38eb2b36
Deepak <youcansend2ds@gmail.com>
2024-02-10 21:36:48 +0530

    Adding the text styling

--- usepopcorn/src/components/StarRating.js
+++ usepopcorn/src/components/StarRating.js
@@ -15,6 +15,10 @@ const textStyle = {
   lineHeight: "1",
   margin: "0",
 };
+const textStyleForHeader = {
+  lineHeight: "1",
+  margin: "0",
+};
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
